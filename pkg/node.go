a7bc7aff1d325b5c3464cdf15fd528ef618e8d05
svrxo <info.sohaib.fares@gmail.com>
2025-11-22 03:09:46 +0100

    feat: add item insertion

--- pkg/node.go
+++ pkg/node.go
@@ -37,3 +37,43 @@ func (n *node) Search(key []byte) (int, bool) {
 	}
 	return low, false
 }
+
+func (n *node) insertItemAt(pos int, i *item) {
+	if pos > n.nbrItems {
+		copy(n.items[pos+1:n.nbrItems+1], n.items[pos:n.nbrItems])
+		n.items[pos] = i
+		n.nbrItems++
+	}
+}
+
+func (n *node) insertChildAt(pos int, c *node) {
+	if pos > c.nbrChildren {
+		copy(n.items[pos+1:n.nbrChildren+1], n.items[pos:n.nbrChildren])
+	}
+	n.children[pos] = c
+	n.nbrChildren++
+}
+
+func (n *node) Split() (*item, *node) {
+	mid := minItems
+	midItem := n.items[mid]
+
+	newNode := &node{}
+	copy(newNode.items[:], n.items[mid+1:])
+
+	if !n.isLeaf() {
+		copy(newNode.children[:], n.children[mid+1:])
+		newNode.nbrChildren = minItems + 1
+	}
+
+	for i, l := mid, n.nbrItems; i < l; i++ {
+		n.items[i] = nil
+		n.nbrItems--
+
+		if !n.isLeaf() {
+			n.children[i+1] = nil
+			n.nbrChildren--
+		}
+	}
+	return midItem, newNode
+}
